0e47f6820d1c7e10f184ca583a4450791991182f
peterncs <89129735+peterncs@users.noreply.github.com>
2021-09-07 15:12:24 +0800

    Change the path of supplier's data to "./supplier-data/images/"

--- supplier_image_upload.py
+++ supplier_image_upload.py
@@ -4,6 +4,6 @@ import requests
 import glob
 
 url = "http://localhost/upload/"
-for image in glob.iglob(r"../supplier-data/images/*.jpeg"):
+for image in glob.iglob(r"./supplier-data/images/*.jpeg"):
     with open(image, 'rb') as opened:
         r = requests.post(url, files={'file': opened})
